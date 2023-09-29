ec704d8d0c14da2aa9aa9c4a00ddd1b10c3c067f
Nicolas Descostes <nicolas.descostes@embl.it>
2023-09-29 11:29:11 +0200

    simplify script and remove testing

--- workflow/scripts/elongationSingleEnd.R
+++ workflow/scripts/elongationSingleEnd.R
@@ -12,10 +12,7 @@ library(csaw)
 
 
 bamfile <- snakemake@input$bamFile
-save(bamfile, file="/g/romebioinfo/Projects/TEbench/workflow/jobs/ChIPSeqProcessing_retrieve_elongationSize_single/bamfile.Rdat")
-stop("saving for testing")
-load("/g/romebioinfo/Projects/TEbench/workflow/jobs/ChIPSeqProcessing_retrieve_elongationSize_single/bamfile.Rdat")
-
+outputfile <- snakemake@output
 
 #############
 ## MAIN
@@ -27,11 +24,11 @@ param <- readParam(minq = 20)
 
 dedupon <- reform(param, dedup=TRUE)
 x <- correlateReads(bamfile, maxdelay, param=dedupon)
-plot(0:maxdelay, x, type = "l", ylab = "CCF", xlab = "Delay (bp)")
+#plot(0:maxdelay, x, type = "l", ylab = "CCF", xlab = "Delay (bp)")
 
 extensionsize <- maximizeCcf(x)
-outputfile <- paste0("results/elongation_size_single_csaw/",
-    strsplit(basename(bamfile), "\\.bam")[[1]], ".txt")
+#outputfile <- paste0("../results/elongation_size_single_csaw/",
+#    strsplit(basename(bamfile), "\\.bam")[[1]], ".txt")
 
 cat("Writing output to: ", outputfile, "\n")
 
