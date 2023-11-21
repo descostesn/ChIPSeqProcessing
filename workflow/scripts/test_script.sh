462f04fdd46dbdb3acb6d604cd4a93bb9177d02d
descoste <nicolas.descostes@embl.it>
2023-11-21 18:38:28 +0100

    increase time

--- workflow/scripts/test_script.sh
+++ workflow/scripts/test_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=100gb
 #SBATCH --job-name testRead
 #SBATCH --ntasks=1
-#SBATCH --time 08:50:00
+#SBATCH --time 15:50:00
 #SBATCH --cpus-per-task=21
 #SBATCH --output slurm_%x_%A_%a.out
 
