c1a59ad04e9306275fe240f11f9ee79a4e336ee2
descoste <nicolas.descostes@embl.it>
2023-11-21 16:35:02 +0100

    change params

--- workflow/scripts/test_script.sh
+++ workflow/scripts/test_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --nodes 1
-#SBATCH --mem=200gb
+#SBATCH --mem=20gb
 #SBATCH --job-name testRead
 #SBATCH --ntasks=1
-#SBATCH --time 04:50:00
+#SBATCH --time 08:50:00
 #SBATCH --output slurm_%x_%A_%a.out
 
 
